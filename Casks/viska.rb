653ae1f3db0d9aa77ae9b07c8edf9f062e85fbf5
PenPlanner <markusanderson81@me.com>
2026-07-23 13:47:36 +0200

    Update Viska to 1.0.13

--- Casks/viska.rb
+++ Casks/viska.rb
@@ -1,6 +1,6 @@
 cask "viska" do
-  version "1.0.10"
-  sha256 "08a6e857cde4e6103f34b0338f3cca205dbb4f2db63dffe79a7a3252be2f3841"
+  version "1.0.13"
+  sha256 "66269d800de9176c25ab9e6ba2ff339bd999b3abb645e21fe8c2ec5305739aab"
 
   url "https://dl.viska-ai.com/Viska-#{version}.pkg"
   name "Viska"
